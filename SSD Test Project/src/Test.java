4db80e41ab5c79eb06eee4aa49b81d4688b32664
Frank Cabada <Frank Cabada>
2017-05-06 14:18:41 -0400

    Updated Test class to test all of our custom detectors so far

--- SSD Test Project/src/Test.java
+++ SSD Test Project/src/Test.java
@@ -1,10 +1,26 @@
+/*
+* Test class for Findbugs custom detectors
+*/
 
 public class Test {
 
-	public void test() {
-		int x = 1;
-		while(x == 1) {
-			
-		}
+	// test for special characters in method name
+	public void f_o() {
+		System.out.println("special chars in method name");
 	}
+
+	// test for long method name
+	public void foooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo() {
+		System.out.println("long method name");
+	}
+
+	// test for matching method names with different capitalization
+	public void foo() {
+		System.out.println("lowercase foo");
+	}
+
+	public void fOo() {
+		System.out.println("uppercase foo");
+	}
+
 }
